c9591f521b8e2a66a39221b0014598a5652323a6
CyberKnight007 <togedemaru.1707@gmail.com>
2020-12-04 00:01:00 +0530

    removed stuff

--- config-example.ts
+++ config-example.ts
@@ -1,5 +1,5 @@
 export const config = {
-  token: 'NzI1Mjk4NTY2ODM4NDg1MDUy.XvMs7Q._7GwXBFAbgDk0w1IllxFNLV3_SI',
-  owners: ['771687332734173185'],
+  token: '',
+  owners: [''],
   module: 'https://raw.githubusercontent.com/harmony-org/harmony/main/mod.ts',
 }
